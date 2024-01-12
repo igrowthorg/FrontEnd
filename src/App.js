55db2b1b17069a8969fb0638ca16c009fa83d644
SathyaHemakanthi <kalanisathya12@gmail.com>
2024-01-12 19:01:29 +0530

    route parent in app.js

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './Components/Authontication/Login';
 import Admin from './Components/Admin/Admin';
 import MidwifeComponent from './Components/MidwifeComponent/MidwifeComponent';
+import ParentComponent from './Components/ParentComponent/ParentComponent';
 
 function App() {
   return (
     <Router>
 
       <Routes>
-        <Route path='/auth' element={<Login />} />
+        <Route path='/' element={<Login />} />
         <Route path="/admin" element={<Admin />} />
         <Route path="/Midwife" element={<MidwifeComponent />} />
+        <Route path="/Parent" element={<ParentComponent />} />
       </Routes>
     </Router>
   );
