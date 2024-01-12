237457f11e42971553db29eb49960bc4f4222714
akdaudsha <akdaudsha@gmail.com>
2024-01-12 12:09:47 +0530

    12/01

--- src/Components/cover/cover.jsx
+++ src/Components/cover/cover.jsx
@@ -1,12 +1,41 @@
 import React from 'react'
 import './cover.css'
 import Logo from '../../images/logo192.png'
+import instance from '../../utility/AxiosInstance'
+import { useNavigate } from 'react-router-dom'
 
-export default function cover() {
+export default function Cover() {
 
-    const log_out = () => {
-        localStorage.clear()
-        window.location.reload();
+    const navigation = useNavigate()
+
+    const logout = async () => {
+        try {
+            const res = await instance.post('/admin/logout')
+            console.log(res.data)
+            navigation('/auth')
+        }
+        catch (err) {
+            console.log(err)
+        }
+
+        try {
+            const res = await instance.post('/midwife/logout')
+            console.log(res.data)
+            navigation('/auth')
+        }
+        catch (err) {
+            console.log(err)
+        }
+
+        try {
+            const res = await instance.post('/officer/logout')
+            console.log(res.data)
+            navigation('/auth')
+        }
+        catch (err) {
+            console.log(err)
+        }
+
     }
 
     return (
@@ -16,7 +45,7 @@ export default function cover() {
                 {/* <p> I-Growth</p> */}
                 <p >Monitor Baby Growth & Suggest Advice</p>
             </div>
-            <button style={{ marginRight: '20px' }} onClick={log_out}>Log Out</button>
+            <button style={{ marginRight: '20px' }} onClick={logout}>Log Out</button>
         </div>
     )
 }
