3204036dec8e134149ab404fb5202e863747c0a3
KavindiAnu <132061242+KavindiAnu@users.noreply.github.com>
2024-01-15 16:22:22 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,19 +2,18 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './Components/Authontication/Login';
 import Admin from './Components/Admin/Admin';
 import MidwifeComponent from './Components/MidwifeComponent/MidwifeComponent';
-import ParentComponent from './Components/ParentComponent/ParentComponent';
-import Bmi from './Components/ParentComponent/CalculateBMI/CalculateBMI.jsx';
+import OfficerComponent from './Components/OfficerComponent/OfficerComponent';
 
 function App() {
   return (
     <Router>
 
       <Routes>
-        <Route path='/bmi' element={<Bmi/>}/>
         <Route path='/' element={<Login />} />
+        <Route path='/auth' element={<Login />} />
         <Route path="/admin" element={<Admin />} />
         <Route path="/Midwife" element={<MidwifeComponent />} />
-        <Route path="/Parent" element={<ParentComponent />} />
+        <Route path="/medical-officer" element={<OfficerComponent />} />
       </Routes>
     </Router>
   );
