9e39f3e3627020586f3cd972babbf6d3e725ab4d
SathyaHemakanthi <kalanisathya12@gmail.com>
2024-01-16 12:18:53 +0530

    update midwife

--- src/Components/MidwifeComponent/MidwifeComponent.jsx
+++ src/Components/MidwifeComponent/MidwifeComponent.jsx
@@ -40,8 +40,6 @@ export default function MinwifeComponent() {
           <li onClick={() => setActive('measure')} style={active === 'measure' ? { background: '#fff', color: 'purple', fontWeight: 'bold' } : {}}>Monthly Measure</li>
           <li onClick={() => setActive('baby_detail')} style={active === 'baby_detail' ? { background: '#fff', color: 'purple', fontWeight: 'bold' } : {}}>Baby Detail</li>
           <li onClick={() => setActive('vacc_detail')} style={active === 'vacc_detail' ? { background: '#fff', color: 'purple', fontWeight: 'bold' } : {}}>Vaccine Detail</li>
-          {/* <li onClick={() => setActive('consult_advices')} style={active === 'consult_advices' ? { background: '#fff', color: 'green', fontWeight: 'bold' } : {}}>Consult Advices</li> */}
-          {/* <li onClick={() => setActive('view_development')} style={active === 'view_development' ? { background: '#fff', color: 'green', fontWeight: 'bold' } : {}}>View Development</li> */}
           <li onClick={() => setActive('create_account')} style={active === 'create_account' ? { background: '#fff', color: 'purple', fontWeight: 'bold' } : {}}>Create Account</li>
           <li onClick={() => setActive('add-news')} style={active === 'add-news' ? { background: '#fff', color: 'purple', fontWeight: 'bold' } : {}}>Add News</li>
           <li onClick={() => setActive('news-feeds')} style={active === 'news-feeds' ? { background: '#fff', color: 'purple', fontWeight: 'bold' } : {}}>News Feeds</li>
@@ -52,8 +50,6 @@ export default function MinwifeComponent() {
         active === 'measure' ? <Measure /> :
           active === 'baby_detail' ? <BabyDetails /> :
             active === 'vacc_detail' ? <Vaccine /> :
-              // active === 'consult_advices' ? <p>Consult Advices</p> :
-              // active === 'view_development' ? <p>View Development</p> :
               active === 'create_account' ? <CreateAccount /> :
                 active === 'add-news' ? <AddMidwifeNews /> :
                   active === 'news-feeds' ? <NewsFeed user="midwife" /> : null
