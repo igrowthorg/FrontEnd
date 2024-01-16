8c64eca112d48503a4b967646200c490677bde3d
SathyaHemakanthi <kalanisathya12@gmail.com>
2024-01-16 20:16:00 +0530

    update parent

--- src/Components/ParentComponent/BabyDetails/babyDetails.jsx
+++ src/Components/ParentComponent/BabyDetails/babyDetails.jsx
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from 'react';
-import '../../MidwifeComponent/BabyDetails/BabyDetails.scss'
+import './BabyDetails.scss'
 import { AiFillCloseCircle } from 'react-icons/ai'
 import instance from '../../../utility/AxiosInstance'
 
 
-const GetDevelopment = (props) => {
+const GetVaccine = (props) => {
 
-    const [development, setDevelopment] = useState(null);
+    const [vaccine, setVaccine] = useState(null);
     const [trigger, setTrigger] = useState(false);
 
     const childID = props.childID;
@@ -14,8 +14,8 @@ const GetDevelopment = (props) => {
     useEffect(() => {
         const getData = async () => {
             try {
-                const res = await instance.get(`parent/child/development/${childID}`);
-                setDevelopment(res.data)
+                const res = await instance.get(`parent/vaccine/${childID}`);
+                setVaccine(res.data)
                 console.log(res.data);
             }
             catch (err) {
@@ -25,34 +25,32 @@ const GetDevelopment = (props) => {
         getData()
     }, [childID, trigger])
 
-    const makeAsDone = async (done_development_id, development_id) => {
-        console.log(done_development_id, development_id);
+    const makeAsTaken = async (time_table_id, vaccine_id) => {
+        console.log(time_table_id, vaccine_id);
 
-        // Ask yes no question from user to make as done
-        const userConfirmation = window.confirm("Are you sure you want to make as done?");
+        // Ask yes no question from user to make as taken confirm
+        const userConfirmation = window.confirm("Are you sure you want to make as taken?");
 
         if (userConfirmation) {
             try {
-                const res = await instance.post(`/midwife/child/vaccine/${childID}/${done_development_id}/${development_id}`);
+                const res = await instance.post(`/midwife/child/vaccine/${childID}/${time_table_id}/${vaccine_id}`);
                 console.log(res.data);
                 setTrigger(!trigger);
-                alert("Successfully made as done")
+                alert("Successfully made as taken")
             }
             catch (err) {
                 console.log(err);
-                alert("Can't make as done")
+                alert("Can't make as taken")
             }
         }
+
     }
 
-    if (development !== null) return (
+    if (vaccine !== null) return (
         <div className='vaccine-card-fram'>
-            {development.map((data, index) => (
-                <div className='vaccine-fram' key={index} style={data.status === "eligible" ? { background: '#98fb98' } : data.status === "not_eligible" ? { background: '#dcdcdc' } : data.status === "done" ? { background: '#6495ed' } : null}>
-                    <p className='vaccine_name'>{data.devlopment_name}</p>
-                    {
-                        data.status === "eligible" && <button onClick={() => makeAsDone(data.time_table_id, data.vaccine_id)}>Make as taken</button>
-                    }
+            {vaccine.map((data, index) => (
+                <div className='vaccine-fram' key={index} style={data.status === "eligible" ? { background: '#98fb98' } : data.status === "not_eligible" ? { background: '#dcdcdc' } : data.status === "taken" ? { background: '#6495ed' } : null}>
+                    <p className='vaccine_name'>{data.vaccine_name}</p>
                 </div>
             ))}
         </div>
@@ -60,7 +58,7 @@ const GetDevelopment = (props) => {
     )
 }
 
-export default function BabyDetails() {
+export default function BabyDetail() {
 
     const month = [
         { month: '2M', no: 2 },
@@ -81,12 +79,13 @@ export default function BabyDetails() {
 
     const [apiData, setApiData] = useState([]);
 
+    const [searchQuery, setSearchQuery] = useState('');
     const [selectedMonth, setSelectedMonth] = useState(2);
 
     useEffect(() => {
         const getData = async () => {
             try {
-                const res = await instance.get('/midwife/child');
+                const res = await instance.get('/parent/child');
                 setApiData(res.data);
             }
             catch (err) {
@@ -106,9 +105,18 @@ export default function BabyDetails() {
         setShowDetail(false);
     }
 
+    const handleSearchChange = (event) => {
+        setSearchQuery(event.target.value);
+    };
+
 
     if (apiData !== null) return (
         <div className='baby-details-container'>
+            <div className='babyDetail-top'>
+                <div className='searchbar'>
+                    <input type="text" placeholder="Search.." name="search" className='search' value={searchQuery} onChange={handleSearchChange} />
+                </div>
+            </div>
             <div className='babyDetail-bottom'>
                 <table>
                     <thead>
@@ -118,12 +126,15 @@ export default function BabyDetails() {
                             <td>Child Name</td>
                             <td>Birthday</td>
                             <td>Gender</td>
+                            <td>Gardiant Name</td>
+                            {/* <td>Mobile</td> */}
                             <td>More</td>
                         </tr>
                     </thead>
                     <tbody>
                         {
                             apiData.map((data, index) => {
+                                if ((data.child_id.toString().includes(searchQuery.toLowerCase()))) {
                                     return (
                                         <tr key={index}>
                                             <td>{index + 1}</td>
@@ -131,8 +142,14 @@ export default function BabyDetails() {
                                             <td>{data.child_name}</td>
                                             <td>{data.child_birthday.split('T')[0]}</td>
                                             <td>{data.child_gender}</td>
+                                            <td>{data.guardian_name}</td>
+                                            {/* <td>{data.phone}</td> */}
                                             <td className='crud-btn'>
                                                 <div className='top-detail' onClick={() => handleViewDetail(data)} >View Detail</div>
+                                                {/* <div className='bottom-detail'>
+                                                <div className='update'>Update</div>
+                                                <div className='delete'>Delete</div>
+                                                </div> */}
                                             </td>
 
                                             {showDetail && selectedBaby && selectedBaby.child_id === data.child_id && (
@@ -147,34 +164,35 @@ export default function BabyDetails() {
                                                                     <div className='detail'><h4>Name :</h4>{data.child_name}</div>
                                                                     <div className='detail'><h4>Birthday :</h4>{data.child_birthday.split('T')[0]}</div>
                                                                     <div className='detail'><h4>Gender :</h4>{data.child_gender}</div>
-                                                                    <div className='detail'><h4>Guardian Name :</h4>{data.guardian_name}</div>
+                                                                    <div className='detail'><h4>Gudiunt Name :</h4>{data.guardian_name}</div>
                                                                     <div className='detail'><h4>Address :</h4>{data.address}</div>
-                                                                    <div className='detail'><h4>Guardian Mobile :</h4>{data.phone}</div>
+                                                                    {/* <div className='detail'><h4>Gudiunt Mobile :</h4>{data.phone}</div> */}
                                                                     <div className='detail'><h4>Bron Weight :</h4>{data.child_born_weight}</div>
+                                                                    {/* <div className='detail'><h4>BMI :</h4>---</div>
+                                                        <div className='detail'><h4>Stage :</h4>----</div> */}
                                                                 </div>
                                                             </div>
                                                             <hr style={{ width: '1200px' }} />
                                                             <div className='bottom-section'>
                                                                 <div className='bottom-left'>
-                                                                    <h3>Development Activities</h3>
+                                                                    <h3>Vaccine Detail</h3>
+                                                                    <GetVaccine childID={data.child_id} />
+
+                                                                </div>
+                                                                <hr style={{ height: '400px' }} />
+                                                                <div className='bottom-right'>
+                                                                    <h3>Development Activites</h3>
                                                                     <div className='development-activites-top'>
                                                                         {month.map((data, index) => (
                                                                             <div className={selectedMonth === data.no ? 'month-fram active' : 'month-fram'} key={index} onClick={() => setSelectedMonth(data.no)}>
                                                                                 <p>{data.month}</p>
                                                                             </div>
                                                                         ))}
-                                                                        <GetDevelopment childID={data.child_id} />
                                                                     </div>
-
-                                                                </div>
-                                                                <hr style={{ height: '400px' }} />
-                                                                <div className='bottom-right'>
-                                                                    <h3>Vaccine Details</h3>                                                                 
-                                                                </div>
                                                                     <div className='development-activites-bottom'>
-                                                                        <Vaccine childID={data.child_id}/>
+                                                                        <DevelopmentActivityTable childID={data.child_id} selectedMonth={selectedMonth} />
                                                                     </div>
-                                                                
+                                                                </div>
                                                             </div>
                                                         </div>
                                                     </div>
@@ -182,7 +200,7 @@ export default function BabyDetails() {
                                             )}
                                         </tr>
                                     )
-                                
+                                }
                             })
                         }
                     </tbody>
@@ -192,39 +210,57 @@ export default function BabyDetails() {
     )
 }
 
-const Vaccine = (props) => {
+const DevelopmentActivityTable = (props) => {
     const child_id = props.childID;
+    const selected_month = props.selectedMonth;
 
-    const [vaccine, setVaccine] = useState([]);
+    const [activity, setActivity] = useState([]);
+    const [trigger, setTrigger] = useState(false);
 
     useEffect(() => {
         const getData = async () => {
             try {
-                const res = await instance.get(`/parent/vaccine/${child_id}`);
+                const res = await instance.get(`parent/dev-activity/${child_id}`);
                 console.log(res.data);
-                // setActivity(res.data);
+                setActivity(res.data);
             }
             catch (err) {
                 console.log(err)
             }
         }
         getData()
-    }, [child_id])
+    }, [child_id, selected_month, trigger])
 
-    if (vaccine.length < 1) return (
-        <p>No taken vaccine</p>
+    const MakeDone = async (id) => {
+        try {
+            const res = await instance.post(`parent/dev-activity/${child_id}/${id}`);
+            console.log(res.data);
+            setTrigger(prv => !prv)
+            alert("Successfully made as done")
+        }
+        catch (err) {
+            console.log(err);
+        }
+    }
+
+    if (activity.length < 1) return (
+        <p>No Activities</p>
     )
 
     return (
         <table style={{ width: '80%', height: '20px' }}>
             {
-                vaccine.map((data, index) => {
-                    return (
+                activity.map((data, index) => {
+                    if (data.activity_month === selected_month) return (
                         <tr key={index}>
                             <td className='number'>{index + 1}</td>
-                            <td>{data.vaccine_name}</td>
+                            <td>{data.activity_name}</td>
+                            {
+                                data.done ? <td><button className='done'>Done</button></td> : <td><button className='makeAsDone' onClick={() => MakeDone(data.activity_id)}>Make as Done</button></td>
+                            }
                         </tr>
                     )
+                    return null
                 })
             }
         </table>
