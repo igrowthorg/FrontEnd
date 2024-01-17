bc55fbb04611347bf9a0fb423b995a9bbc23edd0
SathyaHemakanthi <kalanisathya12@gmail.com>
2024-01-17 06:37:51 +0530

    Update babyDetails.jsx

--- src/Components/ParentComponent/BabyDetails/babyDetails.jsx
+++ src/Components/ParentComponent/BabyDetails/babyDetails.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import './BabyDetails.scss'
+import './babyDetails.scss'
 import { AiFillCloseCircle } from 'react-icons/ai'
 import instance from '../../../utility/AxiosInstance'
 
